37c6a113958fa6c5babc8c35011c4dd272a128dd
Justemoi <144348479+Justemoie@users.noreply.github.com>
2025-02-17 22:06:15 +0300

    Encapsulated class AssignmentDto

--- src/main/java/com/example/sms/dto/AssignmentDto.java
+++ src/main/java/com/example/sms/dto/AssignmentDto.java
@@ -26,4 +26,36 @@ public class AssignmentDto {
                     .toList();
         }
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<EmployeeDto> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(List<EmployeeDto> employees) {
+        this.employees = employees;
+    }
 }
