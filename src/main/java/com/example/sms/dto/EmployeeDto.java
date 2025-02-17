6b9abdf09186e0d120830bf7f493b6dfb294918e
Justemoi <144348479+Justemoie@users.noreply.github.com>
2025-02-17 22:01:41 +0300

    Encapsulated class EmployeeDto

--- src/main/java/com/example/sms/dto/EmployeeDto.java
+++ src/main/java/com/example/sms/dto/EmployeeDto.java
@@ -8,14 +8,14 @@ import java.util.List;
 
 @DataAmount
 public class EmployeeDto {
-    public Long id;
+    private Long id;
 
-    public String firstName;
-    public String lastName;
-    public String phone;
-    public String email;
+    private String firstName;
+    private String lastName;
+    private String phone;
+    private String email;
 
-    public List<AssignmentDto> assignments;
+    private List<AssignmentDto> assignments;
 
     public EmployeeDto(Employee employee, boolean includeAssignments) {
         this.id = employee.getId();
@@ -31,5 +31,53 @@ public class EmployeeDto {
                     .map(assignment -> new AssignmentDto(assignment, false))
                     .toList();
         }
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public List<AssignmentDto> getAssignments() {
+        return assignments;
+    }
+
+    public void setAssignments(List<AssignmentDto> assignments) {
+        this.assignments = assignments;
     }
 }
